8e09f100d73a499a89b0b26286f17c6fa6d790d8
Aditya-chetri <64538450+Aditya-chetri@users.noreply.github.com>
2020-06-27 14:33:16 +0530

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -33,18 +33,12 @@ class Launcher{
             push();
             
             stroke(48,22,8);
-            if(pointA.x < 220) {
-                strokeWeight(7);
-                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
-                
-            }
-            else{
-                strokeWeight(3);
-                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
-                
-            }
+            if(pointA.x < 200){ 
+              strokeWeight(7); line(pointA.x - 15, pointA.y, pointB.x -30, pointB.y); 
+              line(pointA.x +15, pointA.y, pointB.x + 30, pointB.y - 3); 
+
+            }else{ strokeWeight(3); line(pointA.x - 10, pointA.y, pointB.x -25, pointB.y); 
+              line(pointA.x + 15, pointA.y, pointB.x + 25, pointB.y - 3); }
            
             
             pop();
